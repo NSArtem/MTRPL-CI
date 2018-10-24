6b521077010016159a379897f2ac345ed3e47aff
Artem Abramov <ar.abramov@gmail.com>
2018-10-24 09:20:32 +0300

    Initial Commit

---
+++ MTRPL-CI/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MTRPL-CI
+//
+//  Created by Artem Abramov on 24/10/2018.
+//  Copyright © 2018 Artem Abramov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
